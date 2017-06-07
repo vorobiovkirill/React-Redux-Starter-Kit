f9ed7bf392161bff3b3860ae83c88ff35b795787
Kirill Vorobiov <mishkacod@gmail.com>
2017-06-07 17:26:51 +0300

    update

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,11 +1,12 @@
-import css from './app.sass';
+import css from '../styles/main.sass';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './app/App';
 
-
 ReactDOM.render(
-  <App />,
+	<div>
+		<App />
+	</div>,
   document.getElementById('root')
 );
--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const config = {
 
-	entry: './src/index.js',
+	entry: './src/scripts/index.js',
 
 	output: {
 		path: path.resolve(__dirname, 'dist'),
@@ -21,6 +21,10 @@ const config = {
 		children: false,
 	},
 
+	resolve: {
+		extensions: ['.js', '.sass', '.scss', '.html']
+	},
+
 	/**
 	 * @link https://webpack.github.io/docs/webpack-dev-server.html
 	 */
@@ -29,6 +33,7 @@ const config = {
 		compress: true,
 		historyApiFallback: true,
 		port: 8080,
+		inline: true,
 		open: true,
 		stats: {
 			colors: true,
@@ -68,13 +73,28 @@ const config = {
 	plugins: [
 
 		new ExtractTextPlugin({
-			filename: 'style.css'
+			filename: 'style.[hash].css'
 		}),
 
 		new HtmlWebpackPlugin({
-			title: 'My React App',
-			template: './src/index.html',
-		})
+			title: 'My React App!!!!',
+			template: './src/index.html.ejs',
+			inject: 'body',
+			minify: {
+				removeComments: true,
+				collapseWhitespace: true,
+				removeRedundantAttributes: true,
+				useShortDoctype: true,
+				removeEmptyAttributes: true,
+				removeStyleLinkTypeAttributes: true,
+				keepClosingSlash: true,
+				minifyJS: true,
+				minifyCSS: true,
+				minifyURLs: true
+			},
+		}),
+
+		new webpack.HotModuleReplacementPlugin()
 	]
 
 };
