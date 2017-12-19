04cd43151e3248495ed1539e8286c88797ceb319
Kirill Vorobiov <mishkacod@gmail.com>
2017-12-19 11:04:50 +0200

    update dependencies, fix bugs, rewrite folder structure

--- src/scripts/app/store/configureStore.js
+++ src/scripts/app/store/configureStore.js
@@ -1,8 +1,9 @@
 import { applyMiddleware, compose, createStore } from 'redux';
 
+import chalk from 'chalk';
 import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
 import logger from 'redux-logger';
-import reducer from '../redusers/reducers';
+import reducer from '../reducers/reducers';
 import thunk from 'redux-thunk';
 
 const reduxDevtools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
@@ -18,6 +19,20 @@ const configureStore = (initialState) => {
 			applyMiddleware(thunk, logger),
 		),
 	);
+
+	if (module.hot) {
+		// Enable Webpack hot module replacement for reducers
+		module.hot.accept('../reducers/reducers', () => {
+			try {
+				const nextRootReducer = require('../reducers/reducers').default;
+				store.replaceReducer(nextRootReducer);
+			} catch (error) {
+				console.error(
+					chalk.red(`==> 😭  Reducer hot reloading error ${error}`),
+				);
+			}
+		});
+	}
 
 	return store;
 };
